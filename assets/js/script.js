988a213f42f29424491d11d3c317120135d78145
IsItIzzet <ohlhjames@gmail.com>
2024-05-30 10:53:04 -0500

    Added the basic fetch function to script.js so that I can begin to build my index.html

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const apiKey = 'ef8d8963d3dd913264b31cf3e23ab326'
+let sampleCities = ['Seattle', 'Austin', 'Orlando']
+
+var storedCities = JSON.parse(localStorage.getItem("storedCities"));
+
+function fetchForecast() {
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey +"&units=imperial"
+    ).then(function (response) {
+   
+    if (response.status != 200) {
+      alert("This city was not found. Please try again.");
+      sampleCities.shift();
+      localStorage.setItem("storedCities", JSON.stringify(sampleCities));
+      window.location.reload();
+    }
+    return (
+      response.json()
+        .then(function (data) {
+          $("#currentCity").text(data.name);
+          var simpleTemp = Math.round(data.main.temp);
+          $("#currentTemp").text(simpleTemp + "°F");
+          var simpleWind = Math.round(data.wind.speed);
+          $("#currentWind").text(simpleWind + " MPH");
+          $("#currentHumidity").text(data.main.humidity + "%");
+        })
+    );
+  });
+}
