8e3b41f8a97015058a4168aea2f31f4cdd1ace36
IsItIzzet <ohlhjames@gmail.com>
2024-05-30 13:47:31 -0500

    Added more logic to the Js file so that more info can be pulled and displayed, as I go along I am also adjusting stylings and html classes so that everything lines up for functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
 const apiKey = 'ef8d8963d3dd913264b31cf3e23ab326'
-let sampleCities = ['Seattle', 'Austin', 'Orlando']
+const sampleCities = ['Seattle', 'Austin', 'Orlando']
+const cityInputEl = document.querySelector('.search-input')
+const submitBtn = document.querySelector('.submit-button')
 
-let storedCities = JSON.parse(localStorage.getItem("storedCities"));
+const storedCities = JSON.parse(localStorage.getItem("storedCities"));
 
-function fetchForecast() {
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey +"&units=imperial"
-    ).then(function (response) {
-   
+const citySubmitHandler = function (event) {
+  event.preventDefault();
+
+  const city = cityInputEl.value.trim();
+
+  if (city) {
+    fetchWeather(city, apiKey)
+    fetchForecast(city,apiKey);
+
+    repoContainerEl.textContent = '';
+    nameInputEl.value = '';
+  } else {
+    alert('Please enter a city!');
+  }
+};
+
+const fetchWeather = function (city,apiKey) {
+  const  fetchURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey +"&units=imperial"
+  fetch(fetchURL)
+  .then(function (response) {
     if (response.status != 200) {
       alert("This city was not found. Please try again.");
       sampleCities.shift();
@@ -16,9 +34,25 @@ function fetchForecast() {
     return (
       response.json()
         .then(function (data) {
+          console.log(data)
           let simpleTemp = Math.round(data.main.temp);
           let simpleWind = Math.round(data.wind.speed);
         })
     );
   });
 }
+
+const fetchForecast = function (city,apiKey) {
+  const  fetchURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey +"&units=imperial"
+  fetch(fetchURL)
+  .then(function (response) {
+    if (response.status != 200) {
+      alert("This city was not found. Please try again.");
+      sampleCities.shift();
+      localStorage.setItem("storedCities", JSON.stringify(sampleCities));
+      window.location.reload();
+    }
+  })
+}
+
+submitBtn.addEventListener('click', citySubmitHandler);
